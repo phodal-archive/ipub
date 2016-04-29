c18a8759f420f9475162b83c56ddb615acf1f423
Fengda HUANG <h@phodal.com>
2016-04-29 17:44:22 +0800

    extract data

---
+++ app/scripts/mock_data.js
@@ -0,0 +1,153 @@
+var MOCK_DATA = {
+  "sha": "9fb037999f264ba9a7fc6274d15fa3ae2ab98312",
+  "url": "https://api.github.com/repos/octocat/Hello-World/trees/9fb037999f264ba9a7fc6274d15fa3ae2ab98312",
+  "tree": [
+    {
+      "path": ".gitignore",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "50a9b4de6d9359174e234a68f59b46b8adc69dd0",
+      "size": 45,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/50a9b4de6d9359174e234a68f59b46b8adc69dd0"
+    },
+    {
+      "path": "CNAME",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "30cc2ca727ba9e725e9518d0e1d5de2fe4f8b12c",
+      "size": 17,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/30cc2ca727ba9e725e9518d0e1d5de2fe4f8b12c"
+    },
+    {
+      "path": "LICENSE",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "3c08dde309438ca60e878cd597e599208f7a0765",
+      "size": 13735,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/3c08dde309438ca60e878cd597e599208f7a0765"
+    },
+    {
+      "path": "Makefile",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "1f29511ae99262e3983c1befec39850a66670b9a",
+      "size": 1377,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/1f29511ae99262e3983c1befec39850a66670b9a"
+    },
+    {
+      "path": "README.md",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "8b3e03e05f6a9c2863e463ebfe3cbfa1d5008723",
+      "size": 18655,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/8b3e03e05f6a9c2863e463ebfe3cbfa1d5008723"
+    },
+    {
+      "path": "README_GITBOOK.md",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "ed1ea442c5699ed955f671bca82865bc73f7feb5",
+      "size": 4287,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/ed1ea442c5699ed955f671bca82865bc73f7feb5"
+    },
+    {
+      "path": "SUMMARY.md",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "fcfddabae60f5f4a83fc06a980ae83d9cd316d7f",
+      "size": 3318,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/fcfddabae60f5f4a83fc06a980ae83d9cd316d7f"
+    },
+    {
+      "path": "assets",
+      "mode": "040000",
+      "type": "tree",
+      "sha": "5ec014a9424eb26c1a0a27917120044fcfa6ea71",
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/trees/5ec014a9424eb26c1a0a27917120044fcfa6ea71"
+    },
+    {
+      "path": "book.json",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "9e26dfeeb6e641a33dae4961196235bdb965b21b",
+      "size": 2,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/9e26dfeeb6e641a33dae4961196235bdb965b21b"
+    },
+    {
+      "path": "build",
+      "mode": "040000",
+      "type": "tree",
+      "sha": "9ba797642b007042ddb1b2a6f7d1768bb3986b83",
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/trees/9ba797642b007042ddb1b2a6f7d1768bb3986b83"
+    },
+    {
+      "path": "chapters",
+      "mode": "040000",
+      "type": "tree",
+      "sha": "06354a6fe448e295f6b9e01aebf32732f323abac",
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/trees/06354a6fe448e295f6b9e01aebf32732f323abac"
+    },
+    {
+      "path": "cover.jpg",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "a578e2fe2e9a83562259082258dcf47b344c65ff",
+      "size": 477011,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/a578e2fe2e9a83562259082258dcf47b344c65ff"
+    },
+    {
+      "path": "css",
+      "mode": "040000",
+      "type": "tree",
+      "sha": "e0853f7bd2a0fa30cdcdc0c1f70bd7da4298a3b4",
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/trees/e0853f7bd2a0fa30cdcdc0c1f70bd7da4298a3b4"
+    },
+    {
+      "path": "epub.css",
+      "mode": "100755",
+      "type": "blob",
+      "sha": "4d1816ae56451963d5cdc5dd057e68384950003a",
+      "size": 2550,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/4d1816ae56451963d5cdc5dd057e68384950003a"
+    },
+    {
+      "path": "growth.md",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "feb706808b0fffa509d107845ffefb3fe34cc776",
+      "size": 261856,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/feb706808b0fffa509d107845ffefb3fe34cc776"
+    },
+    {
+      "path": "img",
+      "mode": "040000",
+      "type": "tree",
+      "sha": "36912a6c56ad7d0053d8d13bb7d223f62cb0032f",
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/trees/36912a6c56ad7d0053d8d13bb7d223f62cb0032f"
+    },
+    {
+      "path": "index.html",
+      "mode": "100644",
+      "type": "blob",
+      "sha": "06a01cb8fa72c5e85d5f1bbc559b566abc29c425",
+      "size": 368483,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/06a01cb8fa72c5e85d5f1bbc559b566abc29c425"
+    },
+    {
+      "path": "style.css",
+      "mode": "100755",
+      "type": "blob",
+      "sha": "d9ded694d71dd65ffecdcd6cc065eb29b9c15595",
+      "size": 2937,
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/blobs/d9ded694d71dd65ffecdcd6cc065eb29b9c15595"
+    },
+    {
+      "path": "template",
+      "mode": "040000",
+      "type": "tree",
+      "sha": "fee9346442d6faf5394c9c60e1918841472e634c",
+      "url": "https://api.github.com/repos/phodal/growth-ebook/git/trees/fee9346442d6faf5394c9c60e1918841472e634c"
+    }
+  ],
+  "truncated": false
+}
