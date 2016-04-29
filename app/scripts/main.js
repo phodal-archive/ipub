5eb24c0a964f3b118c9f6593d5a4431d2b95e316
Fengda HUANG <h@phodal.com>
2016-04-29 15:27:12 +0800

    add jstree

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,8 +11,19 @@ var editor = new Pen(options);
 var reposName = 'phodal/growth-ebook/';
 var githubUrl = 'https://api.github.com/repos/';
 var gitFiles = 'git/trees/master';
-var url =
 
-$.get(githubUrl + reposName + gitFiles, function(result){
+$.get(githubUrl + reposName + gitFiles, function (result) {
   console.log(result);
 })
+
+$(function () {
+  $('#jstree').jstree();
+  $('#jstree').on("changed.jstree", function (e, data) {
+    console.log(data.selected);
+  });
+  $('button').on('click', function () {
+    $('#jstree').jstree(true).select_node('child_node_1');
+    $('#jstree').jstree('select_node', 'child_node_1');
+    $.jstree.reference('#jstree').select_node('child_node_1');
+  });
+});
