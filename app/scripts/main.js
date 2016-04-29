d8205a922cc72d431a8e587f74344bcd6f4070dd
Fengda HUANG <h@phodal.com>
2016-04-29 14:40:34 +0800

    Create projects

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+var options = {
+  editor: document.body, // {DOM Element} [required]
+  class: 'pen', // {String} class of the editor,
+  debug: false, // {Boolean} false by default
+  textarea: '<textarea name="content"></textarea>', // fallback for old browsers
+  list: ['bold', 'italic', 'underline'] // editor menu list
+}
+
+var editor = new Pen(options);
